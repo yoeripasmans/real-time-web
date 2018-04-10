4acf6f2273a57cf42c6e6224e61f1caf59ee65ac
Yoeri Pasmans <yoerip@live.nl>
2018-04-10 13:55:26 +0200

    heroku fix

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ io.on('connection', function(socket) {
     });
 });
 
-http.listen(3000, function() {
+http.listen(process.env.PORT || 3000, function() {
     console.log('server is running on port 3000');
 });
